aacbfb6ae69c9df339c1184a9f7f71c4b0f4b061
Austin Astorga <astorgaaustin@gmail.com>
2018-05-08 23:27:50 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,14 +11,14 @@ let package = Package(
             targets: ["Menkyo"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.1")
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Menkyo",
-            dependencies: ["Vapor"]),
+            dependencies: ["NIOOpenSSL"]),
         .testTarget(
             name: "MenkyoTests",
             dependencies: ["Menkyo"]),
